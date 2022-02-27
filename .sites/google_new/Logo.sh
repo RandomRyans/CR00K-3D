8502bec9c351186277c2b2e18224e716822b5b58
Random Ryan <rynixupdates@gmail.com>
2022-02-27 01:17:13 -0800

    final
    finished

---
+++ .sites/google_new/Logo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}] ${RED}[${WHITE}FINAL${RED}]${GREEN} TARGETED 2FA SECRET ${RED}[${WHITE}2FA BYPASS${RED}]  ${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]
+${RED}[${WHITE}+${RED}]                                                                   ${RED}[${WHITE}+${RED}]
+${RED}[${WHITE}+${RED}]${RED} ██████╗██████╗ ${WHITE} ██████╗  ██████╗ ${RED}██╗  ██╗       ${RED} ██████╗ ${WHITE}██████╗  ${RED}[${WHITE}+${RED}]
+${RED}[${WHITE}+${RED}]${RED}██╔════╝██╔══██╗${WHITE}██╔═████╗██╔═████╗${RED}██║ ██╔╝       ${RED} ╚════██╗${WHITE}██╔══██╗ ${RED}[${WHITE}+${RED}]
+${RED}[${WHITE}+${RED}]${RED}██║     ██████╔╝${WHITE}██║██╔██║██║██╔██║${RED}█████╔╝ ${WHITE} █████${RED}   █████╔╝${WHITE}██║  ██║ ${RED}[${WHITE}+${RED}]
+${RED}[${WHITE}+${RED}]${RED}██║     ██╔══██╗${WHITE}████╔╝██║████╔╝██║${RED}██╔═██╗        ${RED}  ╚═══██╗${WHITE}██║  ██║ ${RED}[${WHITE}+${RED}]
+${RED}[${WHITE}+${RED}]${RED}╚██████╗██║  ██║${WHITE}╚██████╔╝╚██████╔╝${RED}██║  ██╗       ${RED} ██████╔╝${WHITE}██████╔╝ ${RED}[${WHITE}+${RED}]
+${RED}[${WHITE}+${RED}]${RED} ╚═════╝╚═╝  ╚═╝${WHITE} ╚═════╝  ╚═════╝ ${RED}╚═╝  ╚═╝       ${RED} ╚═════╝ ${WHITE}╚═════╝  ${RED}[${WHITE}+${RED}]
+${RED}[${WHITE}+${RED}]                                                                   ${RED}[${WHITE}+${RED}]
+${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}] ${RED}[${WHITE}FINAL${RED}]${GREEN} TARGETED 2FA SECRET ${RED}[${WHITE}2FA BYPASS${RED}]  ${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]${RED}[${WHITE}+${RED}]
+${RED}[${WHITE}+${RED}]                                                                   ${RED}[${WHITE}+${RED}]
+${RED}[${WHITE}+${RED}]                                                                   ${RED}[${WHITE}+${RED}]
